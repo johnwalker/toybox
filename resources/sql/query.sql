929f8e21907d04bd5b46099ca344dac812dd383e
John L. Walker <john.lou.walker@gmail.com>
2015-04-11 15:41:12 -0400

    wip

--- resources/sql/query.sql
+++ resources/sql/query.sql
@@ -1,10 +1,10 @@
 -- name: create-useraccount!
 -- Creates the useraccount table.
 create table useraccount (
-       userid serial,
+       useraccountid serial,
        username VARCHAR(255) UNIQUE NOT NULL,
        password VARCHAR(255) NOT NULL,
-       PRIMARY KEY (userid)
+       PRIMARY KEY (useraccountid)
 );
 
 -- name: create-urd!
@@ -19,13 +19,14 @@ create table urd (
 -- Creates the userrole table. 
 create table userrole (
        userroleid serial,
-       userid int references user(userid),
+       useraccountid int references user(useraccountid),
        urdid int references urd(urdid),
        primary key (serial)
 );
 
 -- name: create-item!
--- Creates the item table.
+-- Creates the item table for containing item name, price
+-- and quantity tuples.
 create table item (
        itemid serial,
        itemname varchar(255),
@@ -34,8 +35,35 @@ create table item (
        PRIMARY KEY(itemid)
 );
 
+-- name: create-orderitem!
+-- Creates the orderitem table for containing the items
+-- in an order.
+create table orderitem (
+       orderitemid serial,
+       orderid int references order(orderid),
+       itemid int references item(itemid),
+       quantity int,
+       price int,
+       coupon varchar(255),
+       PRIMARY KEY(orderitemid)
+);
+
+create table status (
+       statusid serial,
+       name varchar(255)
+);
+
 -- name: create-order!
--- Creates the order table.
+-- Creates the order table for recording the dates orders
+-- have been placed and their status.
+
+create table order (
+       orderid serial,
+       useraccountid int references useraccount(useraccountid),
+       statusid int references status(statusid)
+       placementtime timestamp,
+       PRIMARY KEY(orderid)
+);
 
 -- name: insert-user!
 -- Inserts a user with the given username and password.
@@ -57,13 +85,13 @@ and
 
 -- name: find-user
 -- Selects for a user with the given username.
-SELECT userid, username, password
+SELECT useraccountid, username, password
 FROM useraccount
 where username = :username
 
 -- name: select-user
 -- Select all users.
-SELECT userid, username, password
+SELECT useraccountid, username, password
 FROM useraccount
 
 -- name: select-item
